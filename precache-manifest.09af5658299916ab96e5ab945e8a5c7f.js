03cf9bb98fd7c2d1a226f27cca58e97e015bdd43
Satyaprakash verma <59075705+blue-run@users.noreply.github.com>
2020-06-09 19:50:54 +0530

    Updates

---
+++ precache-manifest.09af5658299916ab96e5ab945e8a5c7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db0ce942435c1f94a80b4d623017d266",
+    "url": "/To-Do-App/index.html"
+  },
+  {
+    "revision": "ec8325ea14f6f731901b",
+    "url": "/To-Do-App/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f5413c3f450c80cd3bfe",
+    "url": "/To-Do-App/static/css/main.22c7088e.chunk.css"
+  },
+  {
+    "revision": "ec8325ea14f6f731901b",
+    "url": "/To-Do-App/static/js/2.6257fc46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-App/static/js/2.6257fc46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5413c3f450c80cd3bfe",
+    "url": "/To-Do-App/static/js/main.3620c448.chunk.js"
+  },
+  {
+    "revision": "d265e926b70995470301",
+    "url": "/To-Do-App/static/js/runtime-main.bca33665.js"
+  }
+]);
